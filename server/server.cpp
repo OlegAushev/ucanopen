858eae4f6d37558e1e2d373e1929b8f1efd43cf5
Oleg Aushev <aushevom@gmail.com>
2023-03-13 12:30:02 +0300

    add ipc flags asserts

--- server/server.cpp
+++ server/server.cpp
@@ -10,6 +10,13 @@ Server::Server(mcu::ipc::traits::singlecore, mcu::ipc::traits::primary, const Ip
 			nodeId, can_module, object_dictionary, object_dictionary_size)
 	, emb::c28x::interrupt_invoker_array<Server, mcu::can::peripheral_count>(this, can_module->peripheral().underlying_value())
 {
+	assert(ipc_flags.rpdo1_received.mode() == mcu::ipc::Mode::singlecore);
+	assert(ipc_flags.rpdo2_received.mode() == mcu::ipc::Mode::singlecore);
+	assert(ipc_flags.rpdo3_received.mode() == mcu::ipc::Mode::singlecore);
+	assert(ipc_flags.rpdo4_received.mode() == mcu::ipc::Mode::singlecore);
+	assert(ipc_flags.rsdo_received.mode() == mcu::ipc::Mode::singlecore);
+	assert(ipc_flags.tsdo_ready.mode() == mcu::ipc::Mode::singlecore);
+
 	heartbeat_service = new HeartbeatService(this, emb::chrono::milliseconds(1000));
 	tpdo_service = new TpdoService(this);
 	rpdo_service = new RpdoService(this, ipc_flags);
@@ -34,6 +41,13 @@ Server::Server(mcu::ipc::traits::dualcore, mcu::ipc::traits::primary, const IpcF
 	: impl::Server(mcu::ipc::traits::dualcore(), mcu::ipc::traits::primary(), nodeId, can_module)
 	, emb::c28x::interrupt_invoker_array<Server, mcu::can::peripheral_count>(this, can_module->peripheral().underlying_value())
 {
+	assert(ipc_flags.rpdo1_received.mode() == mcu::ipc::Mode::dualcore);
+	assert(ipc_flags.rpdo2_received.mode() == mcu::ipc::Mode::dualcore);
+	assert(ipc_flags.rpdo3_received.mode() == mcu::ipc::Mode::dualcore);
+	assert(ipc_flags.rpdo4_received.mode() == mcu::ipc::Mode::dualcore);
+	assert(ipc_flags.rsdo_received.mode() == mcu::ipc::Mode::dualcore);
+	assert(ipc_flags.tsdo_ready.mode() == mcu::ipc::Mode::dualcore);
+
 	heartbeat_service = new HeartbeatService(this, emb::chrono::milliseconds(1000));
 	tpdo_service = new TpdoService(this);
 	rpdo_service = new RpdoService(this, ipc_flags);
@@ -58,6 +72,13 @@ Server::Server(mcu::ipc::traits::dualcore, mcu::ipc::traits::secondary, const Ip
 	: impl::Server(mcu::ipc::traits::dualcore(), mcu::ipc::traits::secondary(), can_peripheral, object_dictionary, object_dictionary_size)
 	, emb::c28x::interrupt_invoker_array<Server, mcu::can::peripheral_count>(this, can_peripheral.underlying_value())
 {
+	assert(ipc_flags.rpdo1_received.mode() == mcu::ipc::Mode::dualcore);
+	assert(ipc_flags.rpdo2_received.mode() == mcu::ipc::Mode::dualcore);
+	assert(ipc_flags.rpdo3_received.mode() == mcu::ipc::Mode::dualcore);
+	assert(ipc_flags.rpdo4_received.mode() == mcu::ipc::Mode::dualcore);
+	assert(ipc_flags.rsdo_received.mode() == mcu::ipc::Mode::dualcore);
+	assert(ipc_flags.tsdo_ready.mode() == mcu::ipc::Mode::dualcore);
+
 	rpdo_service = new RpdoService(this, ipc_flags);
 	sdo_service = new SdoService(this, ipc_flags);
 }
